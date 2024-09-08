9d2b3734c3e82544bf287bf6f749bced07b3fd24
지상일 <gsl0505@naver.com>
2024-09-08 17:46:59 +0900

    feat: project 초기 세팅

---
+++ ryokouikitai/src/main/java/com/example/ryokouikitai/RyokouikitaiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ryokouikitai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyokouikitaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RyokouikitaiApplication.class, args);
+    }
+
+}
---
+++ ryokouikitai/src/test/java/com/example/ryokouikitai/RyokouikitaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ryokouikitai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RyokouikitaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
