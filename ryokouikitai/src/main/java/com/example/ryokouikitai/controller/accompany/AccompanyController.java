fd21dd2f54cf01902726e34a8a85115850b8770a
sangil <gsl0505@naver.com>
2024-09-25 20:06:11 +0900

    feat: 동행자 상세 페이지 구현

--- ryokouikitai/src/main/java/com/example/ryokouikitai/controller/accompany/AccompanyController.java
+++ ryokouikitai/src/main/java/com/example/ryokouikitai/controller/accompany/AccompanyController.java
@@ -3,6 +3,7 @@ package com.example.ryokouikitai.controller.accompany;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,5 +21,11 @@ public class AccompanyController {
     @GetMapping("/write")
     public String getWriteAccompanyPage() {
         return "accompany/write";
+    }
+
+
+    @GetMapping("/detail/{accompany-id}")
+    public String getBoard1Detail(@PathVariable("accompany-id") String accompanyId) {
+        return "accompany/detail";
     }
 }
