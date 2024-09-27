f8086a1a1c0a0c5f83abb1bfa03c8cb1778e0d3d
sangil <gsl0505@naver.com>
2024-09-27 17:33:21 +0900

    feat: main에서 채팅 미리보기

--- ryokouikitai/src/main/java/com/example/ryokouikitai/chat/ChatController.java
+++ ryokouikitai/src/main/java/com/example/ryokouikitai/chat/ChatController.java
@@ -1,20 +1,26 @@
 package com.example.ryokouikitai.chat;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
-@Controller
+@RestController
 @Slf4j
 public class ChatController {
-    @GetMapping("/chat")
-    public String chatGet(Model model) {
-        log.info("ChatController Test");
+    @Autowired
+    private RedisService redisService;
 
-        model.addAttribute("name", UUID.randomUUID().toString());
-        return "chat";
+    // 지역별로 최근 채팅 기록을 가져오는 API
+    @GetMapping("/chat/{area}/recent")
+    @ResponseBody
+    public List<String> getRecentMessages(@PathVariable("area") String area) {
+        return redisService.getMessages(area, 5);  // 최근 5개의 메시지만 가져오기
     }
 }
